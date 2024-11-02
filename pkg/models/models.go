4e77e490913152272407a8c07a0345b2d8b895bb
João Colonna <joao.colonna@outlook.com>
2024-11-02 13:47:26 -0300

    fix: Corrige atributo tipo Time da model Tb_Task

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,22 +1,24 @@
 package models
 
+import "time"
+
 type Tb_Task struct{
-	tsk_id int `json: "tsk_id"`
-	tsk_name string `json: "tsk_name"`
-	tsk_description string `json: "tsk_description"`
-	tsk_creation_date time.Now() `json: "tsk_creation_date"`
-	tsk_update_date time.Now() `json: "task_update_date"`
-	tsk_deadline_date time.Time `json: "taks_deadline_date"`
-	tsk_status string `json: "tsk_status"`
-	tsk_color string `json: "tsk_color"`
+	tsk_id 				int 		`json: "tsk_id"`
+	tsk_name 			string 		`json: "tsk_name"`
+	tsk_description 	string 		`json: "tsk_description"`
+	tsk_creation_date 	time.Time 	`json: "tsk_creation_date"`
+	tsk_update_date 	time.Time 	`json: "task_update_date"`
+	tsk_deadline_date 	time.Time 	`json: "taks_deadline_date"`
+	tsk_status 			string 		`json: "tsk_status"`
+	tsk_color 			string 		`json: "tsk_color"`
 	// usr_id id `json: "usr_id"`
-	tskpr_id int `json: "tskpr_id"`
+	tskpr_id 			int 		`json: "tskpr_id"`
 }
 
 type Tb_Category struct{
-	cat_id int `json: "cat_id"`
-	cat_name string `json: "cat_name"`
-	usr_id int `json: "usr_id"`
+	cat_id 		int 	`json: "cat_id"`
+	cat_name 	string 	`json: "cat_name"`
+	usr_id 		int 	`json: "usr_id"`
 }
 
 type task_category struct{
